609c8b7848df5d22561f2db525211d1f3b4a47ec
유경 <eeheueklf@gmail.com>
2024-06-10 15:09:01 +0900

    Add: 프로젝트 빌드

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+// import PrivateRoute from "./Components/PrivateRoute";
+// import SignIn from "./pages/SignIn";
+// import SignUp from "./pages/SignUp";
+// import MyPage from "./pages/MyPage";
+// import Input from "./pages/Input";
+// import Result from "./pages/Result";
+// import Stats from "./pages/Stats";
+// import Admin from "./pages/Admin";
+// import GradReqChange from "./pages/GradReqChange";
+// import AddNewSemester from "./pages/AddNewSemster";
+// import CreateDataSet from "./pages/CreateDataSet";
+import { createTheme, ThemeProvider } from "@mui/material";
+import './App.css';
+
+const theme = createTheme({
+  typography: {
+    fontFamily: "'NanumSquare', 'KBO-Dia-Gothic', sans-serif"
+  }
+})
+
+const App = () => {
+  return (
+      <ThemeProvider theme={theme}>
+          <Routes>
+            <Route path="/" element={<Home />} exact={true} />
+            {/* <Route path="/signin" element={<SignIn />} />
+            <Route path="/signup" element={<SignUp />} />
+            <Route element={<PrivateRoute />}>
+              <Route path="/mypage" element={<MyPage />} />
+              <Route path="/input" element={<Input />} />
+              <Route path="/result" element={<Result />} />
+              <Route path="/stats" element={<Stats />} />
+              <Route path="/admin" element={<Admin />} />
+              <Route path="/change" element={<GradReqChange />} />
+              <Route path="/newsem" element={<AddNewSemester />} />
+              <Route path="/dataset" element={<CreateDataSet />} />
+            </Route> */}
+          </Routes>
+      </ThemeProvider>
+  );
+};
+
+export default App;
---
+++ src/pages/Home.js
@@ -0,0 +1,98 @@
+import React from 'react';
+// import { Link } from 'react-router-dom';
+// import Box from '@mui/material/Box'
+// import Stack from '@mui/material/Stack';
+// import LogoutOutlinedIcon from '@mui/icons-material/LogoutOutlined';
+// import AccountCircleRoundedIcon from '@mui/icons-material/AccountCircleRounded';
+// import "./css/Home.css";
+// import { useState, useEffect } from 'react';
+// import LoadingSpinner from '../Components/LoadingSpinner';
+
+const Home = () => {
+    // const [loading, setLoading] = useState(true);
+    // const [isSignIn, setIsSignIn] = useState(false)
+    // const onClickLogout = () => {
+    //     sessionStorage.clear()
+    //     window.location.replace('/')
+    // }
+
+    // useEffect(() => {
+    //     setLoading(false)
+    //     console.log(sessionStorage.getItem('userId'))
+    //     if (sessionStorage.getItem('userId') === null) {
+    //         // 로그인 안 되었다면
+    //         setIsSignIn(false)
+    //     }
+    //     else {
+    //         // 로그인 되었다면
+    //         setIsSignIn(true)
+    //     }
+    // }, [])
+
+
+    return (
+        <>
+        <div>웅냥냥</div>
+        <div>옴뇸뇸</div>
+            {
+                
+            
+            /* {loading ? <LoadingSpinner op={true} /> : (
+                <>
+                <div className="home_box">
+                    <div className="title_area">
+                        <div className="logo">
+                            <img className="logo_img" alt="YouCanGraduate" src="img/logo.png"></img>
+                        </div>
+                        <span className="title">졸업할 수 있을까?</span>
+                        <div className="link">
+                            <a href="https://github.com/CSID-DGU/2022-1-OSSP2-turning-7">기존 프로젝트🎓 / </a>
+                            <a href="https://github.com/CSID-DGU/2023-1-OPPS1-1921-5">1921⭐</a>
+                        </div>
+                    </div>
+                    <Stack className="btn_area" spacing={1}>
+                        {!isSignIn &&
+                            <Link to='/signin'>
+                                <button className="btn">로그인</button>
+                            </Link>
+                        }
+                        {isSignIn &&
+                            <>
+                                <Link to='/input'>
+                                    <button className="btn">입력</button>
+                                </Link>
+                                <Link to='/result'>
+                                    <button className="btn">결과</button>
+                                </Link>
+                                <Link to='/stats'>
+                                    <button className="btn">통계</button>
+                                </Link>
+                            </>
+                        }
+                    </Stack>
+                </div>
+                {isSignIn &&
+                    <>
+                    <Box className="signout_home" title="로그아웃">
+                        <Link to="/" style={{ color: 'black', textDecoration: 'none' }}>
+                            <Stack direction="row" onClick={onClickLogout}>
+                                <LogoutOutlinedIcon /><div className="tool_title">로그아웃</div>
+                            </Stack>
+                        </Link>
+                    </Box>
+                    <Box className="mypage_home" title="마이페이지">
+                        <Link to="/mypage" style={{ color: 'black', textDecoration: 'none' }}>
+                            <Stack direction="row">
+                                <AccountCircleRoundedIcon /><div className="tool_title">마이페이지</div>
+                            </Stack>
+                        </Link>
+                    </Box>
+                    </>
+                }
+                </>
+            )} */}
+        </>
+    );
+};
+
+export default Home;
